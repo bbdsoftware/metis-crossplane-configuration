a1a2c577baba77155bea8c5bcf5ba5444e221982
Ricardo Pinto <pinto.ricky@gmail.com>
2021-08-27 11:42:40 +0100

    Adding metis compositions

---
+++ examples/providers/providers-config/provider-heml.sh
@@ -0,0 +1,3 @@
+SA=$(kubectl -n crossplane-system get sa -o name | grep provider-helm | sed -e 's|serviceaccount\/|crossplane-system:|g')
+kubectl create clusterrolebinding provider-helm-admin-binding --clusterrole cluster-admin --serviceaccount="${SA}"
+kubectl apply -f provider-config-helm.yaml
